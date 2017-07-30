93ffbe9320273e8b22dc975158d43283244f2570
scrudden <og.crudden@gmail.com>
2017-07-30 14:01:58 +0100

    Run tests configs in orders.

--- bin/process_avl.sh
+++ bin/process_avl.sh
@@ -8,4 +8,5 @@ find /usr/local/transitime/config/ -type f -exec sed -i s#"AGENCYNAME"#"$AGENCYN
 find /usr/local/transitime/config/ -type f -exec sed -i s#"GTFSRTVEHICLEPOSITIONS"#"$GTFSRTVEHICLEPOSITIONS"#g {} \;
 
 # This will process AVL using the set of transitime config files in the test directory.
-find /usr/local/transitime/config/test/ -type f -exec java -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
+find /usr/local/transitime/config/test/ -type f | sort -n | xargs -I {} java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
+
