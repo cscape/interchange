e8327fb7db3bf31a56f812af5babbe9ad4b6c40a
scrudden <og.crudden@gmail.com>
2017-08-15 20:25:52 +0100

    Runs base version of transiTime against HART data. It runs 4 days of data running UpdateTravelTimes between each day.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM maven:3.3-jdk-8
-MAINTAINER Nathan Walker <nathan@rylath.net>
+MAINTAINER Nathan Walker <nathan@rylath.net>, Sean Óg Crudden <og.crudden@gmail.com>
 
 ENV TRANSITIMECORE /transitime-core
-ENV PGPASSWORD=transitime
-ENV AGENCYNAME=CAPMETRO
-ENV AGENCYID=1
-ENV GTFS_URL="https://data.texas.gov/download/r4v4-vz24/application/zip"
-ENV GTFSRTVEHICLEPOSITIONS="https://data.texas.gov/download/eiei-9rpf/application/octet-stream"
+
+#ENV PGPASSWORD=transitime
+#ENV AGENCYNAME=CAPMETRO
+#ENV AGENCYID=1
+#ENV GTFS_URL="https://data.texas.gov/download/r4v4-vz24/application/zip"
+#ENV GTFSRTVEHICLEPOSITIONS="https://data.texas.gov/download/eiei-9rpf/application/octet-stream"
 
 RUN apt-get update \
 	&& apt-get install -y postgresql-client \
@@ -61,7 +62,7 @@ RUN git clone https://github.com/scrudden/core.git /transitime-core
 WORKDIR /transitime-core
 
 #RUN git checkout kalman_predictions
-RUN git checkout VIA
+RUN git checkout baselinetest
 
 #RUN git checkout shade_build_upstream
 
@@ -93,7 +94,7 @@ RUN cp /transitime-core/transitime/target/classes/ddl_postgres*.sql /usr/local/t
 # RUN rm -rf /transitime-core
 # RUN rm -rf ~/.m2/repository
 
-# Scripts required to start transiTime. These are in the order they need to be run.
+# Scripts required to start transiTime.
 ADD bin/check_db_up.sh check_db_up.sh
 ADD bin/generate_sql.sh generate_sql.sh
 ADD bin/create_tables.sh create_tables.sh
@@ -104,6 +105,8 @@ ADD bin/start_transitime.sh start_transitime.sh
 ADD bin/get_api_key.sh get_api_key.sh
 ADD bin/import_avl.sh import_avl.sh
 ADD bin/process_avl.sh process_avl.sh
+ADD bin/update_traveltimes.sh update_traveltimes.sh
+ADD bin/set_config.sh set_config.sh
 
 # Handy utility to allow you connect directly to database
 ADD bin/connect_to_db.sh connect_to_db.sh
--- bin/process_avl.sh
+++ bin/process_avl.sh
@@ -8,5 +8,15 @@ find /usr/local/transitime/config/ -type f -exec sed -i s#"AGENCYNAME"#"$AGENCYN
 find /usr/local/transitime/config/ -type f -exec sed -i s#"GTFSRTVEHICLEPOSITIONS"#"$GTFSRTVEHICLEPOSITIONS"#g {} \;
 
 # This will process AVL using the set of transitime config files in the test directory.
-find /usr/local/transitime/config/test/ -type f | sort -n | xargs -I {} java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
+#find /usr/local/transitime/config/test/ -type f | sort -n | xargs -I {} java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-10-2017
 
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-11-2017
+
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-12-2017
+
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-13-2017
