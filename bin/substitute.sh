7be72b49d3c1182e5eb61abc848325f7f7b1b780
ciolt <cioltdev@outlook.com>
2019-07-16 00:40:03 -0400

    update sh script

--- bin/substitute.sh
+++ bin/substitute.sh
@@ -1,7 +1,11 @@
-export ConfigPropsFile="/usr/local/transitclock/config/${AGENCYID}.properties"
+set -u
+export ConfigPropsFile=/usr/local/transitclock/config/$AGENCYID.properties
+echo "Setting substitutes for $AGENCYID at $ConfigPropsFile"
 
 # This is to substitute into config file the env values.
 sed -i -e "s|POSTGRES_PORT_5432_TCP_ADDR|${POSTGRES_PORT_5432_TCP_ADDR}|g" $ConfigPropsFile
 sed -i -e "s|POSTGRES_PORT_5432_TCP_PORT|${POSTGRES_PORT_5432_TCP_PORT}|g" $ConfigPropsFile
 sed -i -e "s|PGPASSWORD|${PGPASSWORD}|g" $ConfigPropsFile
 sed -i -e "s|DATABASE_NAME|TC_AGENCY_${AGENCYID}|g" $ConfigPropsFile
+
+echo "Finished setting substitutes for $AGENCYID"
