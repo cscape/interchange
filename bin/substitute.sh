623195e8a045f4028d8bdde1a128c6722f6c639a
ciolt <cioltdev@outlook.com>
2019-07-16 11:34:26 -0400

    maybe this will make substitute.sh work

--- bin/substitute.sh
+++ bin/substitute.sh
@@ -2,10 +2,10 @@ export ConfigPropsFile=/usr/local/transitclock/config/$AGENCYID.properties
 echo "Setting substitutes for $AGENCYID at $ConfigPropsFile"
 
 # This is to substitute into config file the env values.
-sed -i -e "s|POSTGRES_PORT_5432_TCP_ADDR|${POSTGRES_PORT_5432_TCP_ADDR}|g" $ConfigPropsFile
-sed -i -e "s|POSTGRES_PORT_5432_TCP_PORT|${POSTGRES_PORT_5432_TCP_PORT}|g" $ConfigPropsFile
-sed -i -e "s|PGPASSWORD|${PGPASSWORD}|g" $ConfigPropsFile
-sed -i -e "s|DATABASE_NAME|TC_AGENCY_${AGENCYID}|g" $ConfigPropsFile
+sed -i "s/POSTGRES_PORT_5432_TCP_ADDR/${POSTGRES_PORT_5432_TCP_ADDR}/g" $ConfigPropsFile
+sed -i "s/POSTGRES_PORT_5432_TCP_PORT/${POSTGRES_PORT_5432_TCP_PORT}/g" $ConfigPropsFile
+sed -i "s/PGPASSWORD/${PGPASSWORD}/g" $ConfigPropsFile
+sed -i "s/DATABASE_NAME/TC_AGENCY_${AGENCYID}/g" $ConfigPropsFile
 
 echo "Finished setting substitutes for $AGENCYID"
 sleep 3
