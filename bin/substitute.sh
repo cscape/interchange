a49fda491778a968ad69a72e5fb357ec43255760
ciolt <cioltdev@outlook.com>
2019-07-16 13:46:44 -0400

    sed is the worst program ever

--- bin/substitute.sh
+++ bin/substitute.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 ConfigPropsFile="/usr/local/transitclock/config/${AGENCYID}.properties"
-echo "Setting substitutes for ${AGENCYID} at ${ConfigPropsFile}"
+echo "Setting substitutes for ${AGENCYID} at $ConfigPropsFile"
 
 # This is to substitute into config file the env values.
-sed -i "s/POSTGRES_PORT_5432_TCP_ADDR/${POSTGRES_PORT_5432_TCP_ADDR}/g" "${ConfigPropsFile}"
-sed -i "s/POSTGRES_PORT_5432_TCP_PORT/${POSTGRES_PORT_5432_TCP_PORT}/g" "${ConfigPropsFile}"
-sed -i "s/PGPASSWORD/${PGPASSWORD}/g" "${ConfigPropsFile}"
-sed -i "s/DATABASE_NAME/TC_AGENCY_${AGENCYID}/g" "${ConfigPropsFile}"
+sed -i "s|POSTGRES_PORT_5432_TCP_ADDR|${POSTGRES_PORT_5432_TCP_ADDR}|g" "$ConfigPropsFile"
+sed -i "s|POSTGRES_PORT_5432_TCP_PORT|${POSTGRES_PORT_5432_TCP_PORT}|g" "$ConfigPropsFile"
+sed -i "s|PGPASSWORD|${PGPASSWORD}|g" "$ConfigPropsFile"
+sed -i "s|DATABASE_NAME|TC_AGENCY_${AGENCYID}|g" "$ConfigPropsFile"
 
 echo "Finished setting substitutes for ${AGENCYID}"
 sleep 3
