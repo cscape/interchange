1563fd24c6a7d83b2a671b61fe56b753f5e5889f
ciolt <cioltdev@outlook.com>
2019-07-16 18:32:37 -0400

    Add note about the API keys

--- bin/create_api_key.sh
+++ bin/create_api_key.sh
@@ -2,6 +2,9 @@
 echo 'THETRANSITCLOCK DOCKER: Create API key.'
 . substitute.sh
 
+# ONLY CREATE THIS API KEY
+# FOR THE PRIMARY CORE
+
 java \
   -cp /usr/local/transitclock/Core.jar org.transitclock.applications.CreateAPIKey \
   -c "${TC_PROPERTIES}" \
--- bin/get_api_key.sh
+++ bin/get_api_key.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# ONLY USE THE PRIMARY AGENCY ID.
+# TO BE USED BY PRIMARY CORE
+
 psql \
   -h "${POSTGRES_PORT_5432_TCP_ADDR}" \
   -p "${POSTGRES_PORT_5432_TCP_PORT}" \
