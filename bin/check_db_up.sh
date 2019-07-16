d8c428eb65c587fbcfc1a78945ebb0e56d91bfaa
ciolt <cioltdev@outlook.com>
2019-07-16 12:29:24 -0400

    fix up shebangs + indentation w/ spaces

--- bin/check_db_up.sh
+++ bin/check_db_up.sh
@@ -6,13 +6,13 @@ RET=1
 SUCCESS=0
 until [ "$RET" -eq "$SUCCESS" ]; do
 
-	psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "SELECT EXTRACT(DAY FROM TIMESTAMP '2001-02-16 20:38:40');"
-	RET="$?"
+  psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres -c "SELECT EXTRACT(DAY FROM TIMESTAMP '2001-02-16 20:38:40');"
+  RET="$?"
 
-	if [ "$RET" -ne "$SUCCESS" ]
-		then
-			echo 'Database is not running.'
-			sleep 10
-	fi
+  if [ "$RET" -ne "$SUCCESS" ]
+    then
+      echo 'Database is not running.'
+      sleep 10
+  fi
 done
 echo 'THETRANSITCLOCK DOCKER: Database is now running.'
