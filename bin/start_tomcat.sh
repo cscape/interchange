d61c6054a29fb65393049330164122882cd5b9a4
ciolt <cioltdev@outlook.com>
2019-07-16 18:32:53 -0400

    add tomcat start script in its own script

---
+++ bin/start_tomcat.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo 'THETRANSITCLOCK DOCKER: Start Tomcat/Catalina'
+
+rmiregistry &
+
+#set the API as an environment variable so we can set in JSP of template/includes.jsp in the transitime webapp
+export APIKEY=$(AGENCYID="${AGENCYID}" . get_api_key.sh)
+export CATALINA_OPTS="$CATALINA_OPTS -Dtransitclock.apikey=${APIKEY}"
+
+/usr/local/tomcat/bin/startup.sh
