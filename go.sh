4150e9e3503f66233915f559e9a68ea62a1ac13d
ciolt <cioltdev@outlook.com>
2019-07-15 20:21:36 -0400

    Cut out unused commands in go.sh

--- go.sh
+++ go.sh
@@ -27,8 +27,4 @@ docker run --name transitclock-server-instance --rm --link transitclock-db:postg
 
 docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_webagency.sh
 
-#docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./import_avl.sh
-
-#docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./process_avl.sh
-
 docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD  -p 8080:8080 transitclock-server  start_transitclock.sh
