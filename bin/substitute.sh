e3a6c2432c30f5c9cfa99f1f44d0bac1f52f8525
ciolt <cioltdev@outlook.com>
2019-07-16 12:38:07 -0400

    attempt #7 at fixing substitute.sh

--- bin/substitute.sh
+++ bin/substitute.sh
@@ -4,10 +4,10 @@ ConfigPropsFile="/usr/local/transitclock/config/${AGENCYID}.properties"
 echo "Setting substitutes for ${AGENCYID} at ${ConfigPropsFile}"
 
 # This is to substitute into config file the env values.
-sed -i "'s/POSTGRES_PORT_5432_TCP_ADDR/${POSTGRES_PORT_5432_TCP_ADDR}/g'" "${ConfigPropsFile}"
-sed -i "'s/POSTGRES_PORT_5432_TCP_PORT/${POSTGRES_PORT_5432_TCP_PORT}/g'" "${ConfigPropsFile}"
-sed -i "'s/PGPASSWORD/${PGPASSWORD}/g'" "${ConfigPropsFile}"
-sed -i "'s/DATABASE_NAME/TC_AGENCY_${AGENCYID}/g'" "${ConfigPropsFile}"
+sed -i "s/POSTGRES_PORT_5432_TCP_ADDR/${POSTGRES_PORT_5432_TCP_ADDR}/g" "${ConfigPropsFile}"
+sed -i "s/POSTGRES_PORT_5432_TCP_PORT/${POSTGRES_PORT_5432_TCP_PORT}/g" "${ConfigPropsFile}"
+sed -i "s/PGPASSWORD/${PGPASSWORD}/g" "${ConfigPropsFile}"
+sed -i "s/DATABASE_NAME/TC_AGENCY_${AGENCYID}/g" "${ConfigPropsFile}"
 
 echo "Finished setting substitutes for ${AGENCYID}"
 sleep 3
