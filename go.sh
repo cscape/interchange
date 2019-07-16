d917855c3b066cb34b17d9741b172542bae63505
ciolt <cioltdev@outlook.com>
2019-07-16 18:51:02 -0400

    Update go.sh to use new builders

--- go.sh
+++ go.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 export PGPASSWORD=transitclock
 export PGUSERNAME=postgres
-Tempagencyid=halifax
-TempProperties="/usr/local/transitclock/config/${Tempagencyid}.properties"
 
 docker stop transitclock-db && docker stop transitclock-server-instance
 docker rm transitclock-db && docker rm transitclock-server-instance
@@ -31,53 +29,23 @@ docker run \
   transitclock-server \
   check_db_up.sh
 
-docker run \
-  --name transitclock-server-instance \
-  --rm \
-  --link transitclock-db:postgres \
-  -e PGPASSWORD="${PGPASSWORD}" \
-  -e AGENCYID="${Tempagencyid}" \
-  transitclock-server \
-  create_tables.sh
-
-docker run \
-  --name transitclock-server-instance \
-  --rm \
-  --link transitclock-db:postgres \
-  -e PGPASSWORD="${PGPASSWORD}" \
-  -e AGENCYID="${Tempagencyid}" \
-  -e TC_PROPERTIES="${TempProperties}" \
-  -e GTFS_URL="http://gtfs.halifax.ca/static/google_transit.zip" \
-  transitclock-server \
-  import_gtfs.sh
-
-docker run \
-  --name transitclock-server-instance \
-  --rm \
-  --link transitclock-db:postgres \
-  -e PGPASSWORD="${PGPASSWORD}" \
-  -e TC_PROPERTIES="${TempProperties}" \
-  transitclock-server \
-  create_api_key.sh
-
-docker run \
-  --name transitclock-server-instance \
-  --rm \
-  --link transitclock-db:postgres \
-  -e PGPASSWORD="${PGPASSWORD}" \
-  -e AGENCYID="${Tempagencyid}" \
-  transitclock-server \
-  create_webagency.sh
-
-# DESCRIPTION:	Start the server
+# Sets up the database and necessary stuff
 docker run \
   --name transitclock-server-instance \
   --rm \
   --link transitclock-db:postgres \
   -e PGPASSWORD="${PGPASSWORD}" \
   -e PGUSERNAME="${PGUSERNAME}" \
-  -e TC_PROPERTIES="${TempProperties}" \
-  -e AGENCYID="${Tempagencyid}" \
+  transitclock-server \
+  agency-looper.runtime.sh
+
+# Starts Tomcat and TransitClock cores
+docker run \
+  --name transitclock-server-instance \
+  --rm \
+  --link transitclock-db:postgres \
+  -e PGPASSWORD="${PGPASSWORD}" \
+  -e PGUSERNAME="${PGUSERNAME}" \
   -p 3020:8080 \
   transitclock-server \
-  start_transitclock.sh
+  agency-looper.start.sh
