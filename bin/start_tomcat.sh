b42cd468a04bd586bbf296189beb6229ab150324
ciolt <cioltdev@outlook.com>
2019-07-16 23:34:10 -0400

    UGH

--- bin/start_tomcat.sh
+++ bin/start_tomcat.sh
@@ -6,19 +6,27 @@ rmiregistry &
 
 # set the API as an environment variable so we can set in JSP of template/includes.jsp in the transitime webapp
 export APIKEY=$(AGENCYID="${AGENCYID}" . get_api_key.sh)
-ALLCONFIGS=$(. get-all-configs.sh)
 
 # Connection info to the PRIMARY AGENCY database for Tomcat
-# export JAVA_OPTS="${JAVA_OPTS} -Dtransitclock.apikey=${APIKEY} -Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
-export CATALINA_OPTS="-Dtransitclock.apikey=${APIKEY} -Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
-# -Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
-# -Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
-# -Dhibernate.connection.url=jdbc:postgresql://${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/ \
-# -Dtransitclock.db.dbUserName=postgres \
-# -Dhibernate.connection.username=postgres \
-# -Dtransitclock.db.dbPassword=${PGPASSWORD} \
-# -Dhibernate.connection.password=${PGPASSWORD} \
-# -Dtransitclock.db.dbType=postgresql"
+export JAVA_OPTS="-Dtransitclock.apikey=${APIKEY} \
+-Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties \
+-Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
+-Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
+-Dtransitclock.db.dbUserName=postgres \
+-Dhibernate.connection.username=postgres \
+-Dtransitclock.db.dbPassword=${PGPASSWORD} \
+-Dhibernate.connection.password=${PGPASSWORD} \
+-Dtransitclock.db.dbType=postgresql"
+
+export CATALINA_OPTS="-Dtransitclock.apikey=${APIKEY} \
+-Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties \
+-Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
+-Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
+-Dtransitclock.db.dbUserName=postgres \
+-Dhibernate.connection.username=postgres \
+-Dtransitclock.db.dbPassword=${PGPASSWORD} \
+-Dhibernate.connection.password=${PGPASSWORD} \
+-Dtransitclock.db.dbType=postgresql"
 
 echo CATALINA_OPTS $CATALINA_OPTS
 
