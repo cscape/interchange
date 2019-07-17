576c11ac483bcca4cdfa39890c1caed7668be297
ciolt <cioltdev@outlook.com>
2019-07-16 21:18:46 -0400

    add allconfigs to tomcat

--- bin/start_tomcat.sh
+++ bin/start_tomcat.sh
@@ -5,10 +5,11 @@ rmiregistry &
 
 # set the API as an environment variable so we can set in JSP of template/includes.jsp in the transitime webapp
 export APIKEY=$(AGENCYID="${AGENCYID}" . get_api_key.sh)
+ALLCONFIGS=$(. get-all-configs.sh)
 
 # Connection info to the PRIMARY AGENCY database for Tomcat
-export JAVA_OPTS="$JAVA_OPTS -Dtransitclock.apikey=${APIKEY} \
--Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
+export JAVA_OPTS="${JAVA_OPTS} -Dtransitclock.apikey=${APIKEY} \
+-Dtransitclock.configFiles=${ALLCONFIGS}"
 # -Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
 # -Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
 # -Dhibernate.connection.url=jdbc:postgresql://${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/ \
