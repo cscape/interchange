2422d4843b1966bbe675b34fcc2e451b7261acbf
ciolt <cioltdev@outlook.com>
2019-07-16 21:33:33 -0400

    Make that WebAgency is created for primary core DB

--- bin/agency-looper.runtime.sh
+++ bin/agency-looper.runtime.sh
@@ -2,20 +2,24 @@
 echo 'Starting agency looper for setting up agencies'
 . substitute.sh
 
+PRIMARY_AGENCY=
 K=0
 # Load in each agency & loop
 for filename in /usr/local/transitclock/agencies/*.env; do
   [ -e "$filename" ] || continue
   . "${filename}"
 
-  AGENCYID="${ID}" . create_tables.sh
-  AGENCYID="${ID}" GTFS_URL="${GTFS}" . import_gtfs.sh
-  AGENCYID="${ID}" . create_webagency.sh
   if [ $K -eq 0 ]
   then
     # Only run Create API Key on the primary agency
     AGENCYID="${ID}" . create_api_key.sh
+    PRIMARY_AGENCY="${ID}"
   fi
+
+  AGENCYID="${ID}" . create_tables.sh
+  AGENCYID="${ID}" GTFS_URL="${GTFS}" . import_gtfs.sh
+  PRIMARY_AGENCY="${PRIMARY_AGENCY}" AGENCYID="${ID}" . create_webagency.sh
+
   K=$((K + 1))
 done
 
--- bin/create_webagency.sh
+++ bin/create_webagency.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 echo 'THETRANSITCLOCK DOCKER: Create WebAgency.'
 
+# WebAgency MUST be created on the primary core
+
 java \
-  -Dtransitclock.db.dbName="agency-${AGENCYID}" \
+  -Dtransitclock.db.dbName="agency-${PRIMARY_AGENCY}" \
   -Dtransitclock.hibernate.configFile="/usr/local/transitclock/config/hibernate.cfg.xml" \
   -Dtransitclock.db.dbHost="${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}" \
   -Dtransitclock.db.dbUserName="postgres" \
@@ -11,7 +13,7 @@ java \
   -cp /usr/local/transitclock/Core.jar org.transitclock.db.webstructs.WebAgency \
   "${AGENCYID}" \
   127.0.0.1 \
-  "agency-${AGENCYID}" \
+  "agency-${PRIMARY_AGENCY}" \
   postgresql \
   "${POSTGRES_PORT_5432_TCP_ADDR}" \
   postgres \
