39a84160fc3266ac5057a0f6af23edcca30550f3
ciolt <cioltdev@outlook.com>
2019-07-16 20:58:15 -0400

    Link to configFiles only in tomcat start script

--- bin/start_tomcat.sh
+++ bin/start_tomcat.sh
@@ -7,15 +7,15 @@ rmiregistry &
 export APIKEY=$(AGENCYID="${AGENCYID}" . get_api_key.sh)
 
 # Connection info to the PRIMARY AGENCY database for Tomcat
-export CATALINA_OPTS="$CATALINA_OPTS -Dtransitclock.db.dbType=postgresql \
--Dtransitclock.apikey=${APIKEY} -Dtransitclock.db.dbName=agency-${AGENCYID} \
--Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
--Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
--Dhibernate.connection.url=jdbc:postgresql://${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/agency-${AGENCYID} \
--Dtransitclock.db.dbUserName=postgres \
--Dhibernate.connection.username=postgres \
--Dtransitclock.db.dbPassword=${PGPASSWORD} \
--Dhibernate.connection.password=${PGPASSWORD} \
--Dtransitclock.db.dbType=postgresql"
+export JAVA_OPTS="$JAVA_OPTS -Dtransitclock.apikey=${APIKEY} \
+-Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
+# -Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
+# -Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
+# -Dhibernate.connection.url=jdbc:postgresql://${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/ \
+# -Dtransitclock.db.dbUserName=postgres \
+# -Dhibernate.connection.username=postgres \
+# -Dtransitclock.db.dbPassword=${PGPASSWORD} \
+# -Dhibernate.connection.password=${PGPASSWORD} \
+# -Dtransitclock.db.dbType=postgresql"
 
-/usr/local/tomcat/bin/startup.sh
+. /usr/local/tomcat/bin/startup.sh
