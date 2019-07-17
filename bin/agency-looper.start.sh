97a886c1abfdaa4cf9ae6cf22fac972d78b0dd54
ciolt <cioltdev@outlook.com>
2019-07-17 00:45:58 -0400

    use secondaryRmiPort prop from configfile not args

--- bin/agency-looper.start.sh
+++ bin/agency-looper.start.sh
@@ -2,18 +2,18 @@
 echo 'Starting up all TransitClock agencies'
 . substitute.sh
 
-K=0
-FIRSTAGENCYID=
+M=0
+FIRSTAGENCYID=""
 # Load in each agency & loop
 for filename in /usr/local/transitclock/agencies/*.env; do
   [ -e "$filename" ] || continue
   . "${filename}"
   AGENCYID="${ID}"
   GTFS_URL="${GTFS}"
-  SECONDARY_RMI="-Dtransitclock.rmi.secondaryRmiPort=0"
+  SECONDARY_RMI=""
   SECONDARY_RMISTATUS=true
 
-  if [ $K -eq 0 ]; then
+  if [ $M -eq 0 ]; then
     # Start Tomcat using the 1st agency's API key
     FIRSTAGENCYID="${ID}"
     SECONDARY_RMISTATUS=false
@@ -27,7 +27,7 @@ for filename in /usr/local/transitclock/agencies/*.env; do
   AGENCYID="${ID}" SECONDARY_RMI="${SECONDARY_RMI}" . start_core.sh
   PRIMARY_AGENCY="${FIRSTAGENCYID}" AGENCYID="${ID}" . create_webagency.sh
 
-  K=$((K + 1))
+  M=$((M + 1))
 done
 
 J=0
