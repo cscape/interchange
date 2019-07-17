a2053b0a1bf7a93c7e10a23020b6b727578b8ab0
ciolt <cioltdev@outlook.com>
2019-07-16 23:26:00 -0400

    fix unbound variable

--- bin/start_tomcat.sh
+++ bin/start_tomcat.sh
@@ -10,7 +10,7 @@ ALLCONFIGS=$(. get-all-configs.sh)
 
 # Connection info to the PRIMARY AGENCY database for Tomcat
 # export JAVA_OPTS="${JAVA_OPTS} -Dtransitclock.apikey=${APIKEY} -Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
-export CATALINA_OPTS="${CATALINA_OPTS} -Dtransitclock.apikey=${APIKEY} -Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
+export CATALINA_OPTS="-Dtransitclock.apikey=${APIKEY} -Dtransitclock.configFiles=/usr/local/transitclock/config/${AGENCYID}.properties"
 # -Dtransitclock.hibernate.configFile=/usr/local/transitclock/config/hibernate.cfg.xml \
 # -Dtransitclock.db.dbHost=${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT} \
 # -Dhibernate.connection.url=jdbc:postgresql://${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/ \
