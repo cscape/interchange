00f226ab43bdf4576e37b71b364f75f1f94fbd42
ciolt <cioltdev@outlook.com>
2019-07-19 17:41:00 -0400

    Add favicon override

--- bin/override-tomcat.sh
+++ bin/override-tomcat.sh
@@ -4,9 +4,12 @@ echo "Overriding default TheTransitClock Tomcat data..."
 SRC="/usr/local/transitclock/config/tomcat"
 WAPPS="${CATALINA_HOME}/webapps"
 
-# Replace root index page
+# Tomcat root
 cp -f "${SRC}/ROOT_index.jsp" "${WAPPS}/ROOT/index.jsp"
+cp -f "${SRC}/favicon.png" "${WAPPS}/ROOT/favicon.png"
+rm -f "${WAPPS}/ROOT/favicon.ico"
 
+# Web App
 cp -f "${SRC}/general.css" "${WAPPS}/web/css/general.css"
 cp -f "${SRC}/index.jsp" "${WAPPS}/web/welcome/index.jsp"
 cp -f "${SRC}/header.jsp" "${WAPPS}/web/template/header.jsp"
